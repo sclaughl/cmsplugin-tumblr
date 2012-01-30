952ed225c3ff6d1ac75e420fae0fcbce73c180f4
Stuart Laughlin <stuart@bistrotech.net>
2012-01-30 17:22:35 -0600

    Made tag filter optional, as it ought to have been.

--- models.py
+++ models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class TumblrPlugin(CMSPlugin):
 	hostname = models.CharField(max_length=50)
-	tag = models.CharField(max_length=50, help_text="Limits to posts with the specified tag.")
+	tag = models.CharField(blank=True, max_length=50, help_text="Limits to posts with the specified tag.")
 	limit = models.SmallIntegerField(default=20, help_text="Number of posts to return (20 max).")
