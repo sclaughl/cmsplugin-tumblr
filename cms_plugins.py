3b7dec69ffe0ac41a73068d3f237034f5bcd5e24
Stuart Laughlin <stuart@bistrotech.net>
2012-01-31 17:41:05 +0000

    fixed last of whitespace issues

--- cms_plugins.py
+++ cms_plugins.py
@@ -4,15 +4,15 @@ from models import TumblrPlugin
 import tumblr
 
 class CMSTumblrPlugin(CMSPluginBase):
-	model = TumblrPlugin
-	name = "Tumblr"
-	render_template = "cms/plugins/tumblr.html"
+    model = TumblrPlugin
+    name = "Tumblr"
+    render_template = "cms/plugins/tumblr.html"
 
-	def render(self, context, instance, placeholder):
-		context.update({
-			'tumblr_avatar' : tumblr.get_avatar(instance.hostname),
-			'tumblr_content' : tumblr.get_posts(instance.hostname, instance.tag, instance.limit),
-		})
-		return context
+    def render(self, context, instance, placeholder):
+        context.update({
+            'tumblr_avatar' : tumblr.get_avatar(instance.hostname),
+            'tumblr_content' : tumblr.get_posts(instance.hostname, instance.tag, instance.limit),
+        })
+        return context
 
 plugin_pool.register_plugin(CMSTumblrPlugin)
