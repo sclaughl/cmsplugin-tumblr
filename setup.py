b370bda4073e931fb054e899162113000343d2f7
Stuart Laughlin <stuart@bistrotech.net>
2012-04-27 14:14:28 -0500

    Working toward getting the package up on PyPI.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='tumblr_plugin',
+    version='1.0',
+    packages=['tumblr_plugin',],
+    url='https://github.com/sclaughl/cmsplugin-tumblr',
+    author='Stuart Laughlin',
+    author_email='stuart@bistrotech.net',
+)
