82f57242c6ff0807032077a27dbcea060c4ada9c
Stuart Laughlin <stuart@bistrotech.net>
2012-04-27 15:54:06 -0500

    Change directory structure for PyPI

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='tumblr_plugin',
+    name='cmsplugin-tumblr',
     version='1.0',
-    packages=['tumblr_plugin',],
+    packages=find_packages(),
     url='https://github.com/sclaughl/cmsplugin-tumblr',
     author='Stuart Laughlin',
     author_email='stuart@bistrotech.net',
+    description='A django-cms plugin that displays the last number of posts of a tumblr user',
+    long_description=open('README.rst').read(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
 )
